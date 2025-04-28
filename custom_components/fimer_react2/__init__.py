fa88f354e596a21feb28003faf85cad96ff98fd1
Federico <fsintu@gmail.com>
2025-04-28 20:33:10 +0200

    Update __init__.py

--- custom_components/fimer_react2/__init__.py
+++ custom_components/fimer_react2/__init__.py
@@ -8,11 +8,12 @@ async def async_setup_entry(hass, config_entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
 
     return True
 
 async def async_unload_entry(hass, config_entry):
-    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
+
     hass.data[DOMAIN].pop(config_entry.entry_id)
     return True
