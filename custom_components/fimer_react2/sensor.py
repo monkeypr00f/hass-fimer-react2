c2428da3a713e40df3938c9e91ba7d2797d2820f
Federico <fsintu@gmail.com>
2025-04-28 20:49:47 +0200

    Update sensor.py

--- custom_components/fimer_react2/sensor.py
+++ custom_components/fimer_react2/sensor.py
@@ -6,27 +6,32 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 
     entities = []
 
-    # Sensori statici
+    # Sensori statici (li creiamo SEMPRE subito)
     for key, description in STATIC_SENSORS.items():
         entities.append(FimerReact2Sensor(coordinator, key, description))
 
-    # Sensori dinamici batterie
-    if coordinator.data:
-        for key in coordinator.data.keys():
-            if "_charge" in key or "_discharge" in key:
-                battery_id, sensor_type = key.split("_", 1)
-                sensor_info = BATTERY_SENSOR_TYPES.get(sensor_type)
-                if sensor_info:
-                    description = {
-                        "name": f"{battery_id} {sensor_type.capitalize()}",
-                        "unit": sensor_info["unit"],
-                        "icon": sensor_info["icon"],
-                        "device_class": sensor_info["device_class"],
-                        "state_class": sensor_info["state_class"],
-                    }
-                    entities.append(FimerReact2Sensor(coordinator, key, description))
-
     async_add_entities(entities)
+
+    # Ora aspettiamo i dati per i sensori dinamici
+    if not coordinator.data:
+        await coordinator.async_request_refresh()
+
+    # Sensori dinamici batterie
+    battery_entities = []
+    for key in coordinator.data.keys():
+        if "_charge" in key or "_discharge" in key:
+            battery_id, sensor_type = key.split("_", 1)
+            sensor_info = BATTERY_SENSOR_TYPES.get(sensor_type)
+            if sensor_info:
+                description = {
+                    "name": f"{battery_id} {sensor_type.capitalize()}",
+                    "unit": sensor_info["unit"],
+                    "icon": sensor_info["icon"],
+                    "device_class": sensor_info["device_class"],
+                }
+                battery_entities.append(FimerReact2Sensor(coordinator, key, description))
+
+    async_add_entities(battery_entities)
 
 class FimerReact2Sensor(SensorEntity):
     def __init__(self, coordinator, key, description):
@@ -59,10 +64,6 @@ class FimerReact2Sensor(SensorEntity):
     @property
     def device_class(self):
         return self._description.get("device_class")
-
-    @property
-    def state_class(self):
-        return self._description.get("state_class")
 
     @property
     def available(self):
