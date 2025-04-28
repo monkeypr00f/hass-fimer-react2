203060e11db7ff6f9ae0dbac725fd7a44df0ef0e
Federico <fsintu@gmail.com>
2025-04-28 17:48:18 +0200

    Update const.py

--- custom_components/fimer_react2/const.py
+++ custom_components/fimer_react2/const.py
@@ -1,9 +1,11 @@
-DOMAIN = "fimer_react2"
-DEFAULT_SCAN_INTERVAL = 30
 
-CONF_HOST = "host"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
-CONF_METER_ID = "meter_id"
-CONF_INVERTER_ID = "inverter_id"
-CONF_BATTERY_IDS = "battery_ids"
+DOMAIN = "fimer_react2"
+SCAN_INTERVAL = 30
+
+URL = "http://192.168.178.105/v1/livedata/"
+
+SENSORS = {
+    "home_today": {"name": "Home Today", "unit": "Wh", "icon": "mdi:home", "device_class": "energy"},
+    "grid_balance": {"name": "Grid Balance", "unit": "Wh", "icon": "mdi:transmission-tower", "device_class": "energy"},
+    "battery_status": {"name": "Battery Status", "unit": "%", "icon": "mdi:battery-medium", "device_class": None},
+}
