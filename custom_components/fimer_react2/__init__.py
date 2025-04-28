d23790e3cc67e8b9094f920dbd8ec7e0e32f01e7
Federico <fsintu@gmail.com>
2025-04-28 17:47:04 +0200

    Update __init__.py

--- custom_components/fimer_react2/__init__.py
+++ custom_components/fimer_react2/__init__.py
@@ -1,21 +1,20 @@
 from .const import DOMAIN
-from .config_flow import FimerConfigFlow
+from .coordinator import FimerReact2Coordinator
 
 async def async_setup_entry(hass, config_entry):
-    hass.data.setdefault(DOMAIN, {})
-    from .coordinator import FimerDataUpdateCoordinator
-
-    coordinator = FimerDataUpdateCoordinator(hass, config_entry)
+    coordinator = FimerReact2Coordinator(hass)
     await coordinator.async_config_entry_first_refresh()
 
+    hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-    # Questo è il fix corretto
-    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
 
     return True
 
 async def async_unload_entry(hass, config_entry):
-    await hass.config_entries.async_unload_platforms(config_entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
     hass.data[DOMAIN].pop(config_entry.entry_id)
     return True
