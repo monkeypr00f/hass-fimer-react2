dc00fead31d0d1ebb4270fac33a225cb0e7c2065
Federico <fsintu@gmail.com>
2025-04-28 18:23:43 +0200

    Update sensor.py

--- custom_components/fimer_react2/sensor.py
+++ custom_components/fimer_react2/sensor.py
@@ -1,12 +1,29 @@
 from homeassistant.components.sensor import SensorEntity
-from .const import DOMAIN, SENSORS
+from .const import DOMAIN, STATIC_SENSORS, BATTERY_SENSOR_TYPES
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     coordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     entities = []
-    for key, description in SENSORS.items():
+
+    # Sensori statici
+    for key, description in STATIC_SENSORS.items():
         entities.append(FimerReact2Sensor(coordinator, key, description))
+
+    # Sensori dinamici batterie
+    if coordinator.data:
+        for key in coordinator.data.keys():
+            if "_charge" in key or "_discharge" in key:
+                battery_id, sensor_type = key.split("_", 1)
+                sensor_info = BATTERY_SENSOR_TYPES.get(sensor_type)
+                if sensor_info:
+                    description = {
+                        "name": f"{battery_id} {sensor_type.capitalize()}",
+                        "unit": sensor_info["unit"],
+                        "icon": sensor_info["icon"],
+                        "device_class": sensor_info["device_class"],
+                    }
+                    entities.append(FimerReact2Sensor(coordinator, key, description))
 
     async_add_entities(entities)
 
@@ -18,7 +35,7 @@ class FimerReact2Sensor(SensorEntity):
 
     @property
     def name(self):
-        return self._description["name"]
+        return self._description.get("name")
 
     @property
     def unique_id(self):
@@ -55,6 +72,6 @@ class FimerReact2Sensor(SensorEntity):
             "model": "REACT2",
             "entry_type": "service",
         }
-    
+
     async def async_update(self):
         await self.coordinator.async_request_refresh()
