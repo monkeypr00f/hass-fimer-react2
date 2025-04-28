ffdcc5f1f74f7f64d7a99ca9c98d5366e304c86f
Federico <fsintu@gmail.com>
2025-04-28 17:55:44 +0200

    Update coordinator.py

--- custom_components/fimer_react2/coordinator.py
+++ custom_components/fimer_react2/coordinator.py
@@ -2,13 +2,17 @@ from datetime import timedelta
 import aiohttp
 import async_timeout
 import logging
+
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from .const import DOMAIN, URL, SCAN_INTERVAL
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+from .const import DOMAIN, SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
 class FimerReact2Coordinator(DataUpdateCoordinator):
-    def __init__(self, hass):
+    def __init__(self, hass, config_entry):
+        self.hass = hass
+        self.config_entry = config_entry
         super().__init__(
             hass,
             _LOGGER,
@@ -17,10 +21,19 @@ class FimerReact2Coordinator(DataUpdateCoordinator):
         )
 
     async def _async_update_data(self):
+        host = self.config_entry.data[CONF_HOST]
+        username = self.config_entry.data.get(CONF_USERNAME, "Admin")
+        password = self.config_entry.data.get(CONF_PASSWORD, "00000000")
+
+        url = f"http://{host}/v1/livedata/"
+
         try:
             async with async_timeout.timeout(10):
+                auth = aiohttp.BasicAuth(username, password)
                 session = aiohttp.ClientSession()
-                async with session.get(URL) as response:
+                async with session.get(url, auth=auth) as response:
+                    if response.status != 200:
+                        raise UpdateFailed(f"HTTP error {response.status}")
                     data = await response.json()
                     await session.close()
                     return data
